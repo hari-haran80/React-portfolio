47ac7c60a0e6b80b7ef93064b82b8e7f43d5c68f
hari haran <hariharan.a69@gmail.com>
2024-06-23 13:26:13 +0400

    Updates

---
+++ precache-manifest.f18aac29b11b809ca3f14bde3939c6ef.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ad9d10c7e589fcb4244b3b833be916",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "70883636f53f6af8465e",
+    "url": "/React-portfolio/static/css/2.8494d3a9.chunk.css"
+  },
+  {
+    "revision": "03af9391a7d778058450",
+    "url": "/React-portfolio/static/css/main.8307ecef.chunk.css"
+  },
+  {
+    "revision": "70883636f53f6af8465e",
+    "url": "/React-portfolio/static/js/2.7a5aa8c0.chunk.js"
+  },
+  {
+    "revision": "03af9391a7d778058450",
+    "url": "/React-portfolio/static/js/main.e632d524.chunk.js"
+  },
+  {
+    "revision": "ee0a708000d28b74b350",
+    "url": "/React-portfolio/static/js/runtime~main.ebbf391c.js"
+  },
+  {
+    "revision": "2966df8859d856ec8f81e1ba57ece4f0",
+    "url": "/React-portfolio/static/media/Designer.2966df88.png"
+  },
+  {
+    "revision": "1de94ec2099d3ce8fedf27424d531857",
+    "url": "/React-portfolio/static/media/hari cv.1de94ec2.pdf"
+  },
+  {
+    "revision": "9bc29db8c4e25cea127b3afd48ec4816",
+    "url": "/React-portfolio/static/media/myImage.9bc29db8.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/React-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/React-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/React-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/React-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
